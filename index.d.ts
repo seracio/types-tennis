7a2826963979c8284bb798c0865c7290531f6d4c
nmondon <nmondon@lefigaro.fr>
2017-06-22 10:57:37 +0200

    fix enum

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,39 @@
 declare namespace Tennis {
 
     interface RawPlayer {
-        uri: string,
-        country: string,
-        name: string,
-        height: number,
-        weight: number,
-        turnedPro: number,
-        hand: 'left' | 'right',
+        uri: string
+        country: string
+        name: string
+        height: number
+        weight: number
+        turnedPro: number
+        hand: "left" | "right"
     }
 
     interface Player extends RawPlayer {
-        birthday: Date,
+        birthday: Date
     }
 
     interface NetworkPlayer extends RawPlayer {
-        birthday: string,
+        birthday: string
     }
 
     interface RawRanking {
-        playerUri: string,
-        playerName: string,
-        rank: number,
-        points: number,
-        pointsDropping: number,
-        tournamentsPlayed: number,
-        type: 'single' | 'double',
+        playerUri: string
+        playerName: string
+        rank: number
+        points: number
+        pointsDropping: number
+        tournamentsPlayed: number
+        type: "single" | "double"
     }
 
     interface Ranking extends RawRanking {
-        date: Date,
+        date: Date
     }
 
     interface NetworkRanking extends RawRanking {
-        date: string,
+        date: string
     }
 }
 
